d19ffded82f8bffc950ff932daf3216571668aca
WillRut17 <78185665+WillRut17@users.noreply.github.com>
2022-09-22 21:40:27 -0400

    minor push it will break scrolling until my next push

--- Assets/Scripts/CameraScroll.cs
+++ Assets/Scripts/CameraScroll.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class CameraScroll : MonoBehaviour
 {
-    public Camera cam;
+    public RectTransform cam;
     public int margin;
     public float scrollSpeed;
     public int maximum;
@@ -23,16 +23,18 @@ public class CameraScroll : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Escape))
             SceneManager.LoadScene("Main Menu");
+
         if ((Input.mousePosition.x > Screen.width - margin && cam.transform.position.x < maximum) || (Input.GetKey(KeyCode.D) && cam.transform.position.x < maximum))
         {
-            cam.transform.position = new Vector3(cam.transform.position.x + scrollSpeed * Time.deltaTime, cam.transform.position.y, cam.transform.position.z);
+            cam.anchoredPosition = new Vector2(cam.anchoredPosition.x - scrollSpeed * Time.deltaTime, cam.anchoredPosition.y);
+            //cam.transform.position = new Vector3(cam.transform.position.x + scrollSpeed * Time.deltaTime, cam.transform.position.y, cam.transform.position.z);
         }
         else if (cam.transform.position.x > maximum)
             cam.transform.position = new Vector3(maximum, cam.transform.position.y, cam.transform.position.z);
 
         if ((Input.mousePosition.x <  margin && cam.transform.position.x > minimum) || (Input.GetKey(KeyCode.A) && cam.transform.position.x > minimum))
         {
-            cam.transform.position = new Vector3(cam.transform.position.x - scrollSpeed * Time.deltaTime, cam.transform.position.y, cam.transform.position.z);
+            cam.transform.position = new Vector3(cam.transform.position.x + scrollSpeed * Time.deltaTime, cam.transform.position.y, cam.transform.position.z);
         }
         else if (cam.transform.position.x < minimum)
             cam.transform.position = new Vector3(minimum, cam.transform.position.y, cam.transform.position.z);
