35bd2d283f30503c536b83431340e1ed6a99882b
WillRut17 <78185665+WillRut17@users.noreply.github.com>
2023-03-24 14:33:00 -0400

    added in all lock animations, cat animations, bed background, poster background, new music

--- Assets/Scripts/LetterLock.cs
+++ Assets/Scripts/LetterLock.cs
@@ -4,12 +4,26 @@ using UnityEngine;
 
 public class LetterLock : MonoBehaviour
 {
-    public GameObject[] Slot1;
-    public GameObject[] Slot2;
-    public GameObject[] Slot3;
-    public GameObject[] Slot4;
-    public GameObject[] Slot5;
-    public GameObject[] Slot6;
+    //public GameObject[] Slot1;
+    //public GameObject[] Slot2;
+    //public GameObject[] Slot3;
+    //public GameObject[] Slot4;
+    //public GameObject[] Slot5;
+    //public GameObject[] Slot6;
+
+    public GameObject Slot1;
+    public GameObject Slot2;
+    public GameObject Slot3;
+    public GameObject Slot4;
+    public GameObject Slot5;
+    public GameObject Slot6;
+
+    private Animator Slot1Anim;
+    private Animator Slot2Anim;
+    private Animator Slot3Anim;
+    private Animator Slot4Anim;
+    private Animator Slot5Anim;
+    private Animator Slot6Anim;
 
     private int currentSlot1;
     private int currentSlot2;
@@ -31,15 +45,34 @@ public class LetterLock : MonoBehaviour
     public AudioClip lockClickClip;
 
     private Coroutine coroutine;
+    private Coroutine slot1Cor;
+    private Coroutine slot2Cor;
+    private Coroutine slot3Cor;
+    private Coroutine slot4Cor;
+    private Coroutine slot5Cor;
+    private Coroutine slot6Cor;
     private bool puzzleComplete;
 
     public GameObject winPar;
     public GameObject particlePos;
     private GameObject tempWinEffect;
+    private bool transitioningSlot1;
+    private bool transitioningSlot2;
+    private bool transitioningSlot3;
+    private bool transitioningSlot4;
+    private bool transitioningSlot5;
+    private bool transitioningSlot6;
 
     // Start is called before the first frame update
     void Start()
     {
+        Slot1Anim = Slot1.GetComponent<Animator>();
+        Slot2Anim = Slot2.GetComponent<Animator>();
+        Slot3Anim = Slot3.GetComponent<Animator>();
+        Slot4Anim = Slot4.GetComponent<Animator>();
+        Slot5Anim = Slot5.GetComponent<Animator>();
+        Slot6Anim = Slot6.GetComponent<Animator>();
+
         lockClickSource = GetComponent<AudioSource>();
         currentSlot1 = 0;
         currentSlot2 = 0;
@@ -48,6 +81,12 @@ public class LetterLock : MonoBehaviour
         currentSlot5 = 0;
         currentSlot6 = 0;
         puzzleComplete = false;
+        transitioningSlot1 = false;
+        transitioningSlot2 = false;
+        transitioningSlot3 = false;
+        transitioningSlot4 = false;
+        transitioningSlot5 = false;
+        transitioningSlot6 = false;
     }
 
     // Update is called once per frame
@@ -58,49 +97,81 @@ public class LetterLock : MonoBehaviour
             switch (LockSlot)
             {
                 case 1:
-                    Slot1[currentSlot1].SetActive(false);
-                    currentSlot1++;
-                    currentSlot1 = CheckSlotNum(currentSlot1);
-                    Slot1[currentSlot1].SetActive(true);
+                    if (!transitioningSlot1)
+                    {
+                        currentSlot1++;
+                        Slot1Anim.SetBool("Go to Next State", true);
+                        transitioningSlot1 = true;
+                        lockClickSource.pitch = Random.Range(0.8f, 1f);
+                        lockClickSource.PlayOneShot(lockClickClip, 1f);
+                        slot1Cor = StartCoroutine(LockSlot1WaitTime());
+                        currentSlot1 = CheckSlotNum(currentSlot1);
+                    }
                     break;
                 case 2:
-                    Slot2[currentSlot2].SetActive(false);
-                    currentSlot2++;
-                    currentSlot2 = CheckSlotNum(currentSlot2);
-                    Slot2[currentSlot2].SetActive(true);
+                    if (!transitioningSlot2)
+                    {
+                        currentSlot2++;
+                        Slot2Anim.SetBool("Go to Next State", true);
+                        transitioningSlot2 = true;
+                        lockClickSource.pitch = Random.Range(0.8f, 1f);
+                        lockClickSource.PlayOneShot(lockClickClip, 1f);
+                        slot2Cor = StartCoroutine(LockSlot2WaitTime());
+                        currentSlot2 = CheckSlotNum(currentSlot2);
+                    }
                     break;
                 case 3:
-                    Slot3[currentSlot3].SetActive(false);
-                    currentSlot3++;
-                    currentSlot3 = CheckSlotNum(currentSlot3);
-                    Slot3[currentSlot3].SetActive(true);
+                    if (!transitioningSlot3)
+                    {
+                        currentSlot3++;
+                        Slot3Anim.SetBool("Go to Next State", true);
+                        transitioningSlot3 = true;
+                        lockClickSource.pitch = Random.Range(0.8f, 1f);
+                        lockClickSource.PlayOneShot(lockClickClip, 1f);
+                        slot3Cor = StartCoroutine(LockSlot3WaitTime());
+                        currentSlot3 = CheckSlotNum(currentSlot3);
+                    }
                     break;
                 case 4:
-                    Slot4[currentSlot4].SetActive(false);
-                    currentSlot4++;
-                    currentSlot4 = CheckSlotNum(currentSlot4);
-                    Slot4[currentSlot4].SetActive(true);
+                    if (!transitioningSlot4)
+                    {
+                        currentSlot4++;
+                        Slot4Anim.SetBool("Go to Next State", true);
+                        transitioningSlot4 = true;
+                        lockClickSource.pitch = Random.Range(0.8f, 1f);
+                        lockClickSource.PlayOneShot(lockClickClip, 1f);
+                        slot4Cor = StartCoroutine(LockSlot4WaitTime());
+                        currentSlot4 = CheckSlotNum(currentSlot4);
+                    }
                     break;
                 case 5:
-                    Slot5[currentSlot5].SetActive(false);
-                    currentSlot5++;
-                    currentSlot5 = CheckSlotNum(currentSlot5);
-                    Slot5[currentSlot5].SetActive(true);
+                    if (!transitioningSlot5)
+                    {
+                        currentSlot5++;
+                        Slot5Anim.SetBool("Go to Next State", true);
+                        transitioningSlot5 = true;
+                        lockClickSource.pitch = Random.Range(0.8f, 1f);
+                        lockClickSource.PlayOneShot(lockClickClip, 1f);
+                        slot5Cor = StartCoroutine(LockSlot5WaitTime());
+                        currentSlot5 = CheckSlotNum(currentSlot5);
+                    }
                     break;
                 case 6:
-                    Slot6[currentSlot6].SetActive(false);
-                    currentSlot6++;
-                    currentSlot6 = CheckSlotNum(currentSlot6);
-                    Slot6[currentSlot6].SetActive(true);
+                    if (!transitioningSlot6)
+                    {
+                        currentSlot6++;
+                        Slot6Anim.SetBool("Go to Next State", true);
+                        transitioningSlot6 = true;
+                        lockClickSource.pitch = Random.Range(0.8f, 1f);
+                        lockClickSource.PlayOneShot(lockClickClip, 1f);
+                        slot6Cor = StartCoroutine(LockSlot6WaitTime());
+                        currentSlot6 = CheckSlotNum(currentSlot6);
+                    }
                     break;
                 default:
                     Debug.Log("incorrect");
                     break;
             }
-
-
-            lockClickSource.pitch = Random.Range(0.8f, 1f);
-            lockClickSource.PlayOneShot(lockClickClip, 1f);
 
             if (currentSlot1 == 1 && currentSlot2 == 2 && currentSlot3 == 3 && currentSlot4 == 4 && currentSlot5 == 5 && currentSlot6 == 1)
             {
@@ -122,24 +193,12 @@ public class LetterLock : MonoBehaviour
 
     public void ResetLock()
     {
-        Slot1[currentSlot1].SetActive(false);
-        Slot2[currentSlot2].SetActive(false);
-        Slot3[currentSlot3].SetActive(false);
-        Slot4[currentSlot4].SetActive(false);
-        Slot5[currentSlot5].SetActive(false);
-        Slot6[currentSlot6].SetActive(false);
         currentSlot1 = 0;
         currentSlot2 = 0;
         currentSlot3 = 0;
         currentSlot4 = 0;
         currentSlot5 = 0;
         currentSlot6 = 0;
-        Slot1[currentSlot1].SetActive(true);
-        Slot2[currentSlot2].SetActive(true);
-        Slot3[currentSlot3].SetActive(true);
-        Slot4[currentSlot4].SetActive(true);
-        Slot5[currentSlot5].SetActive(true);
-        Slot6[currentSlot6].SetActive(true);
     }
 
     public void CHEAT()
@@ -159,5 +218,47 @@ public class LetterLock : MonoBehaviour
         letterCHEATBtn.SetActive(false);
         pinCHEATBtn.SetActive(true);
         Destroy(tempWinEffect);
+    }
+
+    private IEnumerator LockSlot1WaitTime()
+    {
+        yield return new WaitForSeconds(0.54f);
+        Slot1Anim.SetBool("Go to Next State", false);
+        transitioningSlot1 = false;
+    }
+
+    private IEnumerator LockSlot2WaitTime()
+    {
+        yield return new WaitForSeconds(0.54f);
+        Slot2Anim.SetBool("Go to Next State", false);
+        transitioningSlot2 = false;
+    }
+
+    private IEnumerator LockSlot3WaitTime()
+    {
+        yield return new WaitForSeconds(0.54f);
+        Slot3Anim.SetBool("Go to Next State", false);
+        transitioningSlot3 = false;
+    }
+
+    private IEnumerator LockSlot4WaitTime()
+    {
+        yield return new WaitForSeconds(0.54f);
+        Slot4Anim.SetBool("Go to Next State", false);
+        transitioningSlot4 = false;
+    }
+
+    private IEnumerator LockSlot5WaitTime()
+    {
+        yield return new WaitForSeconds(0.54f);
+        Slot5Anim.SetBool("Go to Next State", false);
+        transitioningSlot5 = false;
+    }
+
+    private IEnumerator LockSlot6WaitTime()
+    {
+        yield return new WaitForSeconds(0.54f);
+        Slot6Anim.SetBool("Go to Next State", false);
+        transitioningSlot6 = false;
     }
 }
--- Assets/Scripts/PinLock.cs
+++ Assets/Scripts/PinLock.cs
@@ -4,14 +4,32 @@ using UnityEngine;
 
 public class PinLock : MonoBehaviour
 {
-    public GameObject[] Slot1;
-    public GameObject[] Slot2;
-    public GameObject[] Slot3;
-    public GameObject[] Slot4;
-    public GameObject[] Slot5;
-    public GameObject[] Slot6;
-    public GameObject[] Slot7;
-    public GameObject[] Slot8;
+    //public GameObject[] Slot1;
+    //public GameObject[] Slot2;
+    //public GameObject[] Slot3;
+    //public GameObject[] Slot4;
+    //public GameObject[] Slot5;
+    //public GameObject[] Slot6;
+    //public GameObject[] Slot7;
+    //public GameObject[] Slot8;
+
+    public GameObject Slot1;
+    public GameObject Slot2;
+    public GameObject Slot3;
+    public GameObject Slot4;
+    public GameObject Slot5;
+    public GameObject Slot6;
+    public GameObject Slot7;
+    public GameObject Slot8;
+
+    private Animator Slot1Anim;
+    private Animator Slot2Anim;
+    private Animator Slot3Anim;
+    private Animator Slot4Anim;
+    private Animator Slot5Anim;
+    private Animator Slot6Anim;
+    private Animator Slot7Anim;
+    private Animator Slot8Anim;
 
     private int currentSlot1;
     private int currentSlot2;
@@ -33,15 +51,43 @@ public class PinLock : MonoBehaviour
     public AudioClip lockClickClip;
 
     private Coroutine coroutine;
+    private Coroutine slot1Cor;
+    private Coroutine slot2Cor;
+    private Coroutine slot3Cor;
+    private Coroutine slot4Cor;
+    private Coroutine slot5Cor;
+    private Coroutine slot6Cor;
+    private Coroutine slot7Cor;
+    private Coroutine slot8Cor;
     private bool puzzleComplete;
 
     public GameObject particlePos;
     public GameObject winPar;
     private GameObject tempWinEffect;
 
+    private bool transitioningSlot1;
+    private bool transitioningSlot2;
+    private bool transitioningSlot3;
+    private bool transitioningSlot4;
+    private bool transitioningSlot5;
+    private bool transitioningSlot6;
+    private bool transitioningSlot7;
+    private bool transitioningSlot8;
+
     // Start is called before the first frame update
     void Start()
     {
+        Slot1Anim = Slot1.GetComponent<Animator>();
+        Slot2Anim = Slot2.GetComponent<Animator>();
+        Slot3Anim = Slot3.GetComponent<Animator>();
+        Slot4Anim = Slot4.GetComponent<Animator>();
+        Slot5Anim = Slot5.GetComponent<Animator>();
+        Slot6Anim = Slot6.GetComponent<Animator>();
+        Slot7Anim = Slot7.GetComponent<Animator>();
+        Slot8Anim = Slot8.GetComponent<Animator>();
+
+
+
         puzzleComplete = false;
         lockClickSource = GetComponent<AudioSource>();
         currentSlot1 = 0;
@@ -52,6 +98,14 @@ public class PinLock : MonoBehaviour
         currentSlot6 = 0;
         currentSlot7 = 0;
         currentSlot8 = 0;
+        transitioningSlot1 = false;
+        transitioningSlot2 = false;
+        transitioningSlot3 = false;
+        transitioningSlot4 = false;
+        transitioningSlot5 = false;
+        transitioningSlot6 = false;
+        transitioningSlot7 = false;
+        transitioningSlot8 = false;
     }
 
     // Update is called once per frame
@@ -62,61 +116,105 @@ public class PinLock : MonoBehaviour
             switch (LockSlot)
             {
                 case 1:
-                    Slot1[currentSlot1].SetActive(false);
-                    currentSlot1++;
-                    currentSlot1 = CheckSlotNum(currentSlot1);
-                    Slot1[currentSlot1].SetActive(true);
+                    if (!transitioningSlot1)
+                    {
+                        currentSlot1++;
+                        Slot1Anim.SetBool("Go to Next State", true);
+                        transitioningSlot1 = true;
+                        lockClickSource.pitch = Random.Range(0.8f, 1f);
+                        lockClickSource.PlayOneShot(lockClickClip, 1f);
+                        slot1Cor = StartCoroutine(LockSlot1WaitTime());
+                        currentSlot1 = CheckSlotNum(currentSlot1);
+                    }
                     break;
                 case 2:
-                    Slot2[currentSlot2].SetActive(false);
-                    currentSlot2++;
-                    currentSlot2 = CheckSlotNum(currentSlot2);
-                    Slot2[currentSlot2].SetActive(true);
+                    if (!transitioningSlot2)
+                    {
+                        currentSlot2++;
+                        Slot2Anim.SetBool("Go to Next State", true);
+                        transitioningSlot2 = true;
+                        lockClickSource.pitch = Random.Range(0.8f, 1f);
+                        lockClickSource.PlayOneShot(lockClickClip, 1f);
+                        slot2Cor = StartCoroutine(LockSlot2WaitTime());
+                        currentSlot2 = CheckSlotNum(currentSlot2);
+                    }
                     break;
                 case 3:
-                    Slot3[currentSlot3].SetActive(false);
-                    currentSlot3++;
-                    currentSlot3 = CheckSlotNum(currentSlot3);
-                    Slot3[currentSlot3].SetActive(true);
+                    if (!transitioningSlot3)
+                    {
+                        currentSlot3++;
+                        Slot3Anim.SetBool("Go to Next State", true);
+                        transitioningSlot3 = true;
+                        lockClickSource.pitch = Random.Range(0.8f, 1f);
+                        lockClickSource.PlayOneShot(lockClickClip, 1f);
+                        slot3Cor = StartCoroutine(LockSlot3WaitTime());
+                        currentSlot3 = CheckSlotNum(currentSlot3);
+                    }
                     break;
                 case 4:
-                    Slot4[currentSlot4].SetActive(false);
-                    currentSlot4++;
-                    currentSlot4 = CheckSlotNum(currentSlot4);
-                    Slot4[currentSlot4].SetActive(true);
+                    if (!transitioningSlot4)
+                    {
+                        currentSlot4++;
+                        Slot4Anim.SetBool("Go to Next State", true);
+                        transitioningSlot4 = true;
+                        lockClickSource.pitch = Random.Range(0.8f, 1f);
+                        lockClickSource.PlayOneShot(lockClickClip, 1f);
+                        slot4Cor = StartCoroutine(LockSlot4WaitTime());
+                        currentSlot4 = CheckSlotNum(currentSlot4);
+                    }
                     break;
                 case 5:
-                    Slot5[currentSlot5].SetActive(false);
-                    currentSlot5++;
-                    currentSlot5 = CheckSlotNum(currentSlot5);
-                    Slot5[currentSlot5].SetActive(true);
+                    if (!transitioningSlot5)
+                    {
+                        currentSlot5++;
+                        Slot5Anim.SetBool("Go to Next State", true);
+                        transitioningSlot5 = true;
+                        lockClickSource.pitch = Random.Range(0.8f, 1f);
+                        lockClickSource.PlayOneShot(lockClickClip, 1f);
+                        slot5Cor = StartCoroutine(LockSlot5WaitTime());
+                        currentSlot5 = CheckSlotNum(currentSlot5);
+                    }
                     break;
                 case 6:
-                    Slot6[currentSlot6].SetActive(false);
-                    currentSlot6++;
-                    currentSlot6 = CheckSlotNum(currentSlot6);
-                    Slot6[currentSlot6].SetActive(true);
+                    if (!transitioningSlot6)
+                    {
+                        currentSlot6++;
+                        Slot6Anim.SetBool("Go to Next State", true);
+                        transitioningSlot6 = true;
+                        lockClickSource.pitch = Random.Range(0.8f, 1f);
+                        lockClickSource.PlayOneShot(lockClickClip, 1f);
+                        slot6Cor = StartCoroutine(LockSlot6WaitTime());
+                        currentSlot6 = CheckSlotNum(currentSlot6);
+                    }
                     break;
                 case 7:
-                    Slot7[currentSlot7].SetActive(false);
-                    currentSlot7++;
-                    currentSlot7 = CheckSlotNum(currentSlot7);
-                    Slot7[currentSlot7].SetActive(true);
+                    if (!transitioningSlot7)
+                    {
+                        currentSlot7++;
+                        Slot7Anim.SetBool("Go to Next State", true);
+                        transitioningSlot7 = true;
+                        lockClickSource.pitch = Random.Range(0.8f, 1f);
+                        lockClickSource.PlayOneShot(lockClickClip, 1f);
+                        slot7Cor = StartCoroutine(LockSlot7WaitTime());
+                        currentSlot7 = CheckSlotNum(currentSlot7);
+                    }
                     break;
                 case 8:
-                    Slot8[currentSlot8].SetActive(false);
-                    currentSlot8++;
-                    currentSlot8 = CheckSlotNum(currentSlot8);
-                    Slot8[currentSlot8].SetActive(true);
+                    if (!transitioningSlot8)
+                    {
+                        currentSlot8++;
+                        Slot8Anim.SetBool("Go to Next State", true);
+                        transitioningSlot8 = true;
+                        lockClickSource.pitch = Random.Range(0.8f, 1f);
+                        lockClickSource.PlayOneShot(lockClickClip, 1f);
+                        slot8Cor = StartCoroutine(LockSlot8WaitTime());
+                        currentSlot8 = CheckSlotNum(currentSlot8);
+                    }
                     break;
                 default:
                     Debug.Log("incorrect");
                     break;
             }
-
-
-            lockClickSource.pitch = Random.Range(0.8f, 1f);
-            lockClickSource.PlayOneShot(lockClickClip, 1f);
 
             if (currentSlot1 == 0 && currentSlot2 == 1 && currentSlot3 == 2 && currentSlot4 == 3 && currentSlot5 == 0 && currentSlot6 == 3 && currentSlot7 == 2 && currentSlot8 == 2)
             {
@@ -138,14 +236,6 @@ public class PinLock : MonoBehaviour
 
     public void ResetLock()
     {
-        Slot1[currentSlot1].SetActive(false);
-        Slot2[currentSlot2].SetActive(false);
-        Slot3[currentSlot3].SetActive(false);
-        Slot4[currentSlot4].SetActive(false);
-        Slot5[currentSlot5].SetActive(false);
-        Slot6[currentSlot6].SetActive(false);
-        Slot7[currentSlot7].SetActive(false);
-        Slot8[currentSlot8].SetActive(false);
         currentSlot1 = 0;
         currentSlot2 = 0;
         currentSlot3 = 0;
@@ -154,14 +244,6 @@ public class PinLock : MonoBehaviour
         currentSlot6 = 0;
         currentSlot7 = 0;
         currentSlot8 = 0;
-        Slot1[currentSlot1].SetActive(true);
-        Slot2[currentSlot2].SetActive(true);
-        Slot3[currentSlot3].SetActive(true);
-        Slot4[currentSlot4].SetActive(true);
-        Slot5[currentSlot5].SetActive(true);
-        Slot6[currentSlot6].SetActive(true);
-        Slot7[currentSlot7].SetActive(true);
-        Slot8[currentSlot8].SetActive(true);
     }
 
     public void CHEAT()
@@ -182,5 +264,60 @@ public class PinLock : MonoBehaviour
         lockSlots.SetActive(false);
         refPanel.SetActive(true);
         Destroy(tempWinEffect);
+    }
+    private IEnumerator LockSlot1WaitTime()
+    {
+        yield return new WaitForSeconds(0.54f);
+        Slot1Anim.SetBool("Go to Next State", false);
+        transitioningSlot1 = false;
+    }
+
+    private IEnumerator LockSlot2WaitTime()
+    {
+        yield return new WaitForSeconds(0.54f);
+        Slot2Anim.SetBool("Go to Next State", false);
+        transitioningSlot2 = false;
+    }
+
+    private IEnumerator LockSlot3WaitTime()
+    {
+        yield return new WaitForSeconds(0.54f);
+        Slot3Anim.SetBool("Go to Next State", false);
+        transitioningSlot3 = false;
+    }
+
+    private IEnumerator LockSlot4WaitTime()
+    {
+        yield return new WaitForSeconds(0.54f);
+        Slot4Anim.SetBool("Go to Next State", false);
+        transitioningSlot4 = false;
+    }
+
+    private IEnumerator LockSlot5WaitTime()
+    {
+        yield return new WaitForSeconds(0.54f);
+        Slot5Anim.SetBool("Go to Next State", false);
+        transitioningSlot5 = false;
+    }
+
+    private IEnumerator LockSlot6WaitTime()
+    {
+        yield return new WaitForSeconds(0.54f);
+        Slot6Anim.SetBool("Go to Next State", false);
+        transitioningSlot6 = false;
+    }
+
+    private IEnumerator LockSlot7WaitTime()
+    {
+        yield return new WaitForSeconds(0.54f);
+        Slot7Anim.SetBool("Go to Next State", false);
+        transitioningSlot7 = false;
+    }
+
+    private IEnumerator LockSlot8WaitTime()
+    {
+        yield return new WaitForSeconds(0.54f);
+        Slot8Anim.SetBool("Go to Next State", false);
+        transitioningSlot8 = false;
     }
 }
